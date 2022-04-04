bea4f0256e0a4d5b86a145733e120a1ab5d86f05
Luís Ferro <lfa1ferro@gmail.com>
2022-04-04 20:45:40 +0100

    Fixed git conflicts in custom errors

--- src/errors/databaseError.ts
+++ src/errors/databaseError.ts
@@ -1,11 +1,7 @@
 export class DatabaseError extends Error {
     constructor(message: string) {
         super(message);
-<<<<<<< HEAD
         this.name = this.constructor.name;
-=======
-        this.name = 'DatabaseError';
->>>>>>> e30afaeaa8be549bbae9b62e32153ca2ade16709
         Error.captureStackTrace(this, this.constructor);
     }
 }
--- src/errors/interactionError.ts
+++ src/errors/interactionError.ts
@@ -1,19 +1,7 @@
-<<<<<<< HEAD
 export class InteractionError extends Error {
     constructor(message: string) {
         super(message);
         this.name = this.constructor.name;
-=======
-import { InteractionCategories } from '../types/categories';
-
-export class InteractionError extends Error {
-    public category: InteractionCategories;
-
-    constructor(message: string, category: InteractionCategories = 'COMMAND_INTERACTION') {
-        super(message);
-        this.name = this.constructor.name;
-        this.category = category;
->>>>>>> e30afaeaa8be549bbae9b62e32153ca2ade16709
         Error.captureStackTrace(this, this.constructor);
     }
 }
