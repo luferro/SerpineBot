531c2649c5026736ee100eed89ddc1e981167a79
Luís Ferro <lfa1ferro@gmail.com>
2022-04-04 22:01:06 +0100

    Added a google search scraper

---
+++ src/apis/google.ts
@@ -0,0 +1,19 @@
+import { load } from 'cheerio';
+import { fetch } from '../services/fetch';
+
+export const search = async (query: string) => {
+    const data = await fetch<string>(`https://www.google.com/search?q=${query}`);
+    const $ = load(data);
+
+    return $('#search').children('div').children('div').children().get().map(element => {
+        const name = $(element).find('h3').text();
+        const url = $(element).find('a').attr('href')!;
+
+        if(!name && !url) return;
+
+        return {
+            name,
+            url
+        }
+    }).filter((item): item is NonNullable<typeof item> => !!item);
+}
