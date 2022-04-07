13f3098c6d808e3f0c10d0a59f96b7ead1ab1f60
Luís Ferro <lfa1ferro@gmail.com>
2022-04-07 23:22:47 +0100

    Switched from pino to winston logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,12 +1,19 @@
-import pino from 'pino';
+import { createLogger, format, transports } from 'winston';
 
-export const logger = pino({
-    transport: {
-        depthLimit: 20,
-        target: 'pino-pretty',
-        options: {
-            translateTime: true,
-            ignore: 'pid,hostname'
-        }
-    }
+const { colorize, combine, timestamp, printf } = format;
+
+const stringFormatter = combine(
+    timestamp(),
+    printf(({ timestamp, level, message, stack }) => {
+        const log = `${colorize().colorize(level, `[${timestamp}] ${level.toUpperCase()}:`)} \x1b[36m${message}\x1b[0m`;
+    
+        if(stack) return log.concat('\n').concat(stack);
+        return log;
+    })
+);
+
+export const logger = createLogger({
+    transports: [
+        new transports.Console({ format: stringFormatter })
+    ]
 });
