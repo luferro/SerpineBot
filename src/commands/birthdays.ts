93c0857eded13582edfc674fc4528e075c6fce46
Luís Ferro <lfa1ferro@gmail.com>
2022-04-09 01:20:23 +0100

    Changed output for birthdays list command

--- src/commands/birthdays.ts
+++ src/commands/birthdays.ts
@@ -50,7 +50,18 @@ const getBirthdays = async (interaction: CommandInteraction) => {
     const birthdays = await Birthdays.getBirthdays(interaction.client);
     if(birthdays.length === 0) return await interaction.reply({ content: 'No birthdays have been registered.', ephemeral: true });
 
-    const formattedBirthday = birthdays.map(item => `${item.user.tag} **${item.birthday}**`);
+    const sortedBirthdays = birthdays.sort((a, b) => {
+        const date = new Date();
+        const { 0: firstDateDay, 1: firstDateMonth } = a.birthday.split('/');
+        const { 0: secondDateDay, 1: secondDateMonth } = b.birthday.split('/');
+
+        const firstDate = new Date(date.getFullYear(), Number(firstDateMonth) - 1, Number(firstDateDay));
+        const secondDate = new Date(date.getFullYear(), Number(secondDateMonth) - 1, Number(secondDateDay));
+
+        return firstDate.getTime() - secondDate.getTime();
+    })
+
+    const formattedBirthday = sortedBirthdays.map(item => `> **${item.birthday}** ${item.user.tag}`);
 
     await interaction.reply({ embeds: [
 		new MessageEmbed()
