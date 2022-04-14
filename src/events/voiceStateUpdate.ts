24dcbfc35d10d04faa6c07539f0eded6cb17d6ee
Luís Ferro <lfa1ferro@gmail.com>
2022-04-14 15:10:01 +0100

    Fixed voiceStateUpdate logic to work only when SerpineBot disconnects

--- src/events/voiceStateUpdate.ts
+++ src/events/voiceStateUpdate.ts
@@ -7,6 +7,7 @@ export const data = {
 }
 
 export const execute = async (client: Bot, oldState: VoiceState, newState: VoiceState) => {
-    const newChannel = newState.channel?.isVoice();
-    if(!newChannel) client.music.delete(newState.guild.id);
+    const isSelf = oldState.guild.me?.user.id === oldState.member?.user.id;
+    const isStillInVoiceChannel = newState.channel?.isVoice();
+    if(isSelf && !isStillInVoiceChannel) client.music.delete(newState.guild.id);
 }
