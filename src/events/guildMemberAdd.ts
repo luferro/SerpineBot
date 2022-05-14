1760030585c672d2d0e8d28a4777d148bc5d117d
Luís Ferro <lfa1ferro@gmail.com>
2022-05-14 11:24:25 +0100

    Added a role type assertion

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -1,4 +1,4 @@
-import { GuildMember } from 'discord.js';
+import { GuildMember, Role } from 'discord.js';
 import { Bot } from '../bot';
 import * as Roles from '../services/roles';
 import { logger } from '../utils/logger';
@@ -12,7 +12,7 @@ export const execute = async (client: Bot, member: GuildMember) => {
 	let role = member.guild.roles.cache.find((role) => role.name === 'Restrictions');
 	if (!role) role = await Roles.create(member.guild, 'Restrictions', 'DEFAULT', false, false);
 
-	await member.roles.add(role);
+	await member.roles.add(role as Role);
 
 	logger.info(`User _*${member.user.tag}*_ has joined guild _*${member.guild.name}*_.`);
 };
