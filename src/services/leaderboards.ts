377ec97bb06594db9052c8e3316d544fa4285b95
Luís Ferro <lfa1ferro@gmail.com>
2022-08-07 14:37:28 +0100

    Add a Xbox leaderboard based on gamerscore

--- src/services/leaderboards.ts
+++ src/services/leaderboards.ts
@@ -1,13 +1,10 @@
 import { Client } from 'discord.js';
 import { Bot } from '../bot';
 import * as Steam from '../apis/steam';
+import * as Xbox from '../apis/xbox';
 import { steamModel } from '../database/models/steam';
-
-enum Medals {
-	'🥇' = 1,
-	'🥈',
-	'🥉',
-}
+import { Medals } from '../types/enums';
+import { xboxModel } from '../database/models/xbox';
 
 export const getSteamLeaderboard = async (client: Bot | Client) => {
 	const leaderboard = [];
@@ -15,12 +12,10 @@ export const getSteamLeaderboard = async (client: Bot | Client) => {
 	const integrations = await steamModel.find();
 	for (const integration of integrations) {
 		const data = await Steam.getRecentlyPlayed(integration.profile.id);
-		if (!data) continue;
+		if (data.length === 0) continue;
 
 		const recentlyPlayed = data.map((game) => {
-			const storedItem = integration.recentlyPlayed.find(
-				({ id: nestedStoredItemId }) => nestedStoredItemId === game.id,
-			);
+			const storedItem = integration.recentlyPlayed.find(({ id: storedItemId }) => storedItemId === game.id);
 			const storedWeeklyHours = storedItem?.weeklyHours ?? 0;
 
 			return {
@@ -51,6 +46,37 @@ export const getSteamLeaderboard = async (client: Bot | Client) => {
 		.map(({ user, topPlayed }, index) => {
 			const position = Medals[index + 1] ?? `\`${index + 1}.\``;
 			const description = `**${user.tag}** with \`${topPlayed.hours}h\`\nTop played game was **[${topPlayed.name}](${topPlayed.url})**`;
+
+			return `${position} ${description}`;
+		});
+};
+
+export const getXboxLeaderboard = async (client: Bot | Client) => {
+	const leaderboard = [];
+
+	const integrations = await xboxModel.find();
+	for (const integration of integrations) {
+		const data = await Xbox.getProfile(integration.profile.gamertag);
+		if (!data) continue;
+
+		const weeklyGamerscore = data.gamerscore - integration.profile.gamerscore;
+
+		await steamModel.updateOne({ userId: integration.userId }, { $set: { 'profile.gamerscore': data.gamerscore } });
+
+		const user = await client.users.fetch(integration.userId);
+
+		leaderboard.push({
+			user,
+			gamerscore: weeklyGamerscore,
+		});
+	}
+
+	return leaderboard
+		.sort((a, b) => b.gamerscore - a.gamerscore)
+		.slice(0, 10)
+		.map(({ user, gamerscore }, index) => {
+			const position = Medals[index + 1] ?? `\`${index + 1}.\``;
+			const description = `**${user.tag}** with \`${gamerscore}G\``;
 
 			return `${position} ${description}`;
 		});
