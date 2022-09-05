e780250de221559c9353cd010e43b5c9d044ef45
Luís Ferro <lfa1ferro@gmail.com>
2022-09-05 10:45:05 +0100

    Set undici connect timeout to 60s

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,32 +1,29 @@
-import { request } from 'undici';
+import { Agent, request, setGlobalDispatcher } from 'undici';
 import UserAgent from 'user-agents';
 import { FetchError } from '../errors/fetchError';
 import { HttpMethod } from '../types/enums';
 import { Request } from '../types/request';
-import { logger } from './logger';
+
+setGlobalDispatcher(
+	new Agent({
+		connect: { timeout: 60_000 },
+	}),
+);
 
 export const fetch = async <T>({ method = HttpMethod.GET, url, body }: Request): Promise<T> => {
-	try {
-		const res = await request(url, {
-			method,
-			headers: {
-				'User-Agent': new UserAgent().toString(),
-				'content-type':
-					body instanceof URLSearchParams ? 'application/x-www-form-urlencoded' : 'application/json',
-			},
-			body: body?.toString(),
-		});
+	const res = await request(url, {
+		method,
+		headers: {
+			'User-Agent': new UserAgent().toString(),
+			'content-type': body instanceof URLSearchParams ? 'application/x-www-form-urlencoded' : 'application/json',
+		},
+		body: body?.toString(),
+	});
 
-		if (res.statusCode >= 400) {
-			await res.body.dump();
-			throw new FetchError(`_*${method}*_ request to _*${url}*_ failed. Status code: _*${res.statusCode}*_.`);
-		}
-
-		return res.headers['content-type']?.includes('application/json')
-			? await res.body.json()
-			: await res.body.text();
-	} catch (error) {
-		logger.warn(`_*${method}*_ request to _*${url}*_ failed.`);
-		throw error;
+	if (res.statusCode >= 400) {
+		await res.body.dump();
+		throw new FetchError(`_*${method}*_ request to _*${url}*_ failed. Status code: _*${res.statusCode}*_.`);
 	}
+
+	return res.headers['content-type']?.includes('application/json') ? await res.body.json() : await res.body.text();
 };
