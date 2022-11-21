6f25ebcfcdcfabc95f083fe7ea01fd14caf1e562
Luís Ferro <lfa1ferro@gmail.com>
2022-11-21 21:00:45 +0000

    Migrate to fetch method from undici

--- packages/shared-utils/src/utils/fetch.ts
+++ packages/shared-utils/src/utils/fetch.ts
@@ -1,5 +1,5 @@
 import type { HttpMethod } from '../types/fetch';
-import { request } from 'undici';
+import { fetch as _fetch } from 'undici';
 import UserAgent from 'user-agents';
 import { FetchError } from '../errors/FetchError';
 
@@ -11,24 +11,27 @@ interface Request {
 
 export const fetch = async <T>({ method = 'GET', url, body }: Request): Promise<T> => {
 	try {
-		const res = await request(url, {
+		const res = await _fetch(url, {
 			method,
 			headers: {
 				'User-Agent': new UserAgent().toString(),
 				'content-type':
 					body instanceof URLSearchParams ? 'application/x-www-form-urlencoded' : 'application/json',
 			},
-			body: body?.toString() ?? null,
+			body: body ?? null,
 		});
 
-		if (res.statusCode >= 400) {
-			await res.body.dump();
-			throw new Error(res.statusCode.toString());
+		console.log(`Fetching ${url}...`);
+
+		if (!res.ok) {
+			throw new Error(`${res.status} ${res.statusText}`);
 		}
 
-		return res.headers['content-type']?.includes('application/json')
-			? await res.body.json()
-			: await res.body.text();
+		const data = res.headers.get('content-type')?.includes('application/json')
+			? await res.json()
+			: await res.text();
+
+		return data as unknown as Promise<T>;
 	} catch (error) {
 		throw new FetchError(`**${method}** request to **${url}** failed. Reason: **${(error as Error).message}**.`);
 	}
