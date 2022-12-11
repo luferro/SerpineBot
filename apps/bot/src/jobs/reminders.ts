b10ed39235ac8d9690972275ce9076968b4aa4ce
Luís Ferro <lfa1ferro@gmail.com>
2022-12-11 12:15:40 +0000

    Improve reminders job logic

--- apps/bot/src/jobs/reminders.ts
+++ apps/bot/src/jobs/reminders.ts
@@ -7,25 +7,20 @@ import { logger } from '@luferro/shared-utils';
 
 export const data: JobData = {
 	name: JobName.Reminders,
-	schedule: '*/10 * * * * *',
+	schedule: '*/30 * * * * *',
 };
 
 export const execute = async (client: Bot) => {
 	const reminders = await remindersModel.find().sort({ timeEnd: 'asc' });
-	if (reminders.length === 0) return;
-
-	const {
-		0: { reminderId, userId, timeEnd },
-	} = reminders;
-
-	if (Date.now() < timeEnd) return;
-
-	try {
-		await Reminders.send(client, reminderId);
-		const user = await client.users.fetch(userId);
-		logger.info(`Job **${data.name}** notified **${user.tag}** regarding reminderId **${reminderId}**.`);
-	} catch (error) {
-		const { message } = error as Error;
-		logger.warn(`Job **${data.name}** failed for reminderId **${reminderId}**. **${message}**.`);
+	const filteredReminders = reminders.filter(({ timeEnd }) => Date.now() >= timeEnd);
+	for (const { reminderId, userId } of filteredReminders) {
+		try {
+			await Reminders.send(client, reminderId);
+			const user = await client.users.fetch(userId);
+			logger.info(`Job **${data.name}** notified **${user.tag}** regarding reminderId **${reminderId}**.`);
+		} catch (error) {
+			const { message } = error as Error;
+			logger.warn(`Job **${data.name}** failed for reminderId **${reminderId}**. **${message}**.`);
+		}
 	}
 };
