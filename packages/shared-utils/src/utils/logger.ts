f4c286c5b5d94ec696c5bb4511156d8b3e702da9
Luís Ferro <lfa1ferro@gmail.com>
2022-12-11 12:13:42 +0000

    Improve logger

--- packages/shared-utils/src/utils/logger.ts
+++ packages/shared-utils/src/utils/logger.ts
@@ -2,7 +2,7 @@ import { createLogger, format, transports } from 'winston';
 
 const { colorize, combine, errors, timestamp, printf } = format;
 
-const stringFormatter = combine(
+const formatter = combine(
 	timestamp(),
 	printf(({ timestamp, level, message, stack }) => {
 		const colorizedMessage = message.replace(/\*\*(.*?)\*\*/g, '\x1b[36m$1\x1b[0m');
@@ -10,12 +10,12 @@ const stringFormatter = combine(
 
 		if (!stack) return log;
 
-		const stackTrace = stack.replace('Error:', 'Stack Trace:').replace(message, '');
+		const stackTrace = stack.replace(/^.+:/, 'Stack Trace:').replace(message, '');
 		return `${log}\n${stackTrace}`;
 	}),
 );
 
 export const logger = createLogger({
 	format: errors({ stack: true }),
-	transports: [new transports.Console({ level: 'debug', format: stringFormatter })],
+	transports: [new transports.Console({ level: 'debug', format: formatter })],
 });
