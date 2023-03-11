6006a2fd78adb3663d3d88e33d3a619ce9f1d37b
Luís Ferro <lfa1ferro@gmail.com>
2023-03-11 14:35:42 +0000

    Avoid slash commands deploy when bot joins a guild

--- apps/bot/src/events/guildCreate.ts
+++ apps/bot/src/events/guildCreate.ts
@@ -2,7 +2,6 @@ import { SettingsModel } from '@luferro/database';
 import { logger } from '@luferro/shared-utils';
 import type { Guild } from 'discord.js';
 
-import * as CommandsHandler from '../handlers/commands';
 import type { Bot } from '../structures/bot';
 import type { EventData } from '../types/bot';
 import { EventName } from '../types/enums';
@@ -12,8 +11,7 @@ export const data: EventData = {
 	type: 'on',
 };
 
-export const execute = async (client: Bot, guild: Guild) => {
-	await CommandsHandler.deployCommands(client);
+export const execute = async (_client: Bot, guild: Guild) => {
 	await SettingsModel.createGuildSettings(guild.id, { messages: [], webhooks: [] });
 	logger.info(`Settings for **${guild.name}** have been created.`);
 };
