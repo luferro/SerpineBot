a8e32671b8a9e81567786609f1d625ce203ed54b
Luís Ferro <lfa1ferro@gmail.com>
2023-05-28 21:56:13 +0100

    Minor change on how events and jobs are imported

--- apps/bot/src/handlers/events.ts
+++ apps/bot/src/handlers/events.ts
@@ -1,13 +1,13 @@
 import { FileUtil, logger } from '@luferro/shared-utils';
 import path from 'path';
 
-import { Bot } from '../structures/bot';
+import { Bot } from '../structures/Bot';
 import type { Event } from '../types/bot';
 
 export const registerEvents = async () => {
 	const files = FileUtil.getFiles(path.resolve(__dirname, '../events'));
 	for (const file of files) {
-		const event: Event = await import(`../events/${file}`);
+		const event: Event = await import(file);
 		Bot.events.set(event.data.name, event);
 	}
 
--- apps/bot/src/handlers/jobs.ts
+++ apps/bot/src/handlers/jobs.ts
@@ -1,13 +1,13 @@
 import { FileUtil, logger } from '@luferro/shared-utils';
 import path from 'path';
 
-import { Bot } from '../structures/bot';
+import { Bot } from '../structures/Bot';
 import type { Job } from '../types/bot';
 
 export const registerJobs = async () => {
 	const files = FileUtil.getFiles(path.resolve(__dirname, '../jobs'));
 	for (const file of files) {
-		const job: Job = await import(`../jobs/${file}`);
+		const job: Job = await import(file);
 		Bot.jobs.set(job.data.name, job);
 	}
 
