b7a86687fe8a97222b51d908c23fa6a1e99f00a6
Luís Ferro <lfa1ferro@gmail.com>
2023-05-31 19:03:30 +0100

    Improve fetch util

--- packages/shared-utils/src/utils/fetch.ts
+++ packages/shared-utils/src/utils/fetch.ts
@@ -2,45 +2,34 @@ import { HeaderGenerator } from 'header-generator';
 import undici from 'undici';
 
 import { FetchError } from '../errors/FetchError';
+import { logger } from './logger';
 
 type HttpMethod = 'GET' | 'PUT' | 'POST' | 'PATCH' | 'DELETE';
-type Request = { url: string | URL; method?: HttpMethod; body?: string | URLSearchParams };
+type Request = { url: string | URL; method?: HttpMethod; body?: string };
 
-const generateHeaders = ({ body }: Pick<Request, 'body'>) => {
-	const headers = new HeaderGenerator({ browserListQuery: 'last 5 versions' }).getHeaders();
-
-	if (body) {
-		return {
-			...headers,
-			'content-type': body instanceof URLSearchParams ? 'application/x-www-form-urlencoded' : 'application/json',
-		};
-	}
-
-	return headers;
-};
+const generateHeaders = () => new HeaderGenerator({ browserListQuery: 'last 5 versions' }).getHeaders();
 
 export const fetch = async <T>({ method = 'GET', url, body }: Request) => {
+	const generatedHeaders = generateHeaders();
+	const headers = body ? { ...generatedHeaders, 'content-type': 'application/json' } : generatedHeaders;
+
 	try {
-		const res = await undici.fetch(url, {
-			method,
-			headers: generateHeaders({ body }),
-			body: body ?? null,
-		});
+		const res = await undici.fetch(url, { method, headers, body });
 		if (!res.ok) throw new Error(`${res.status} ${res.statusText}`);
 
 		const data = res.headers.get('content-type')?.includes('application/json')
 			? await res.json()
 			: await res.text();
 
-		return data as unknown as Promise<T>;
+		return data as Promise<T>;
 	} catch (error) {
-		const { message } = error as Error;
-		throw new FetchError(`**${method}** request to **${url}** failed. Reason: **${message}**.`);
+		logger.debug(error);
+		throw new FetchError(`**${method}** request to **${url}** failed.`);
 	}
 };
 
-export const fetchRedirectLocation = async ({ method = 'GET', url }: Omit<Request, 'body'>) => {
-	const res = await undici.request(url, { method, headers: generateHeaders({}) });
+export const getRedirectLocation = async ({ url }: Omit<Request, 'body'>) => {
+	const res = await undici.request(url, { headers: generateHeaders(), method: 'GET' });
 	await res.body.dump();
 	return res.headers['location']?.toString() ?? null;
 };
