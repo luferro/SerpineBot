40bcc556ead425820c352c61b4cda7b90ca9e884
Luís Ferro <lfa1ferro@gmail.com>
2023-06-03 18:22:50 +0100

    Provide alternative way to fetch reviews using url

--- packages/gaming-api/src/api/opencritic/opencritic.api.ts
+++ packages/gaming-api/src/api/opencritic/opencritic.api.ts
@@ -1,24 +1,30 @@
 import { GoogleApi } from '@luferro/google-api';
-import { StringUtil } from '@luferro/shared-utils';
+import { StringUtil, UrlUtil } from '@luferro/shared-utils';
 
-import { Endpoint, getReviewDetails } from './opencritic.scraper';
+import { Endpoint, getReviewsDetails } from './opencritic.scraper';
 
-const getSlugFromId = async (id: string) => {
-	const results = await GoogleApi.engine.search(`${id} site:https://opencritic.com/game`);
-	const slug = results[0]?.url.split('/').at(5) ?? null;
-	return { slug };
+const getUrlParameters = async (query: string) => {
+	const results = await GoogleApi.engine.search(`${query} site:https://opencritic.com/game`);
+	return {
+		id: results[0]?.url.split('/').at(4) ?? null,
+		slug: results[0]?.url.split('/').at(5) ?? null,
+	};
 };
 
 export const search = async (title: string) => {
-	const results = await GoogleApi.engine.search(`${title} site:https://opencritic.com/game`);
-	const id = results[0]?.url.split('/').at(4) ?? null;
+	const { id } = await getUrlParameters(title);
 	return { id };
 };
 
-export const getReviewById = async (id: string) => {
-	const { slug } = await getSlugFromId(id);
+export const getReviewsById = async (id: string) => {
+	const { slug } = await getUrlParameters(id);
 	if (!slug) throw new Error(`Couldn't fetch reviews for game with id ${id}`);
 
 	const url = StringUtil.format<Endpoint>(Endpoint.GAME_PAGE, id, slug);
-	return await getReviewDetails(url);
+	return await getReviewsDetails(url);
+};
+
+export const getReviewsForUrl = async (url: string) => {
+	if (!UrlUtil.isValid(url)) throw new Error('Invalid url.');
+	return await getReviewsDetails(url);
 };
