da41aa4b013bff6b3b8a8a7e19e8723d7fc50036
Luís Ferro <lfa1ferro@gmail.com>
2023-06-04 19:00:00 +0100

    Change how state is stored

--- packages/database/src/database/models/state.ts
+++ packages/database/src/database/models/state.ts
@@ -3,36 +3,24 @@ import mongoose, { Model } from 'mongoose';
 import type { State, StateEntry } from '../../types/schemas';
 
 interface StateModel extends Model<State> {
-	createOrUpdateState: (job: string, category: string, entries: StateEntry[]) => Promise<void>;
+	createOrUpdateState: (job: string, entries: StateEntry[]) => Promise<void>;
 	getStateByJob: (job: string) => Promise<State | null>;
 }
 
 const schema = new mongoose.Schema<State>(
 	{
-		jobName: { type: String, required: true },
-		entries: {
-			type: Map,
-			of: [
-				{
-					title: String,
-					url: String,
-				},
-			],
-		},
+		job: { type: String, required: true },
+		entries: [{ title: String, url: String }],
 	},
 	{ timestamps: true },
 );
 
-schema.statics.createOrUpdateState = async function (job: string, category: string, entries: StateEntry[]) {
-	await this.updateOne(
-		{ jobName: job.toUpperCase() },
-		{ $set: { [`entries.${category}`]: entries.slice(-100) } },
-		{ upsert: true },
-	);
+schema.statics.createOrUpdateState = async function (job: string, entries: StateEntry[]) {
+	await this.updateOne({ job }, { $set: { entries: entries.slice(-100) } }, { upsert: true });
 };
 
 schema.statics.getStateByJob = async function (job: string) {
-	const state = await this.findOne({ jobName: job.toUpperCase() });
+	const state = await this.findOne({ job });
 	return state ?? null;
 };
 
