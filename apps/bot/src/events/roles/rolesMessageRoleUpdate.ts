d410162f2a5a8d4913c8a63578213647c57aad1e
Luís Ferro <lfa1ferro@gmail.com>
2023-06-09 20:19:48 +0100

    Create events for roles message

---
+++ apps/bot/src/events/roles/rolesMessageRoleUpdate.ts
@@ -0,0 +1,59 @@
+import { logger } from '@luferro/shared-utils';
+import { EmbedBuilder } from 'discord.js';
+
+import type { EventData, EventExecute } from '../../types/bot';
+import { ExtendedStringSelectMenuInteraction } from '../../types/interaction';
+
+type Args = [interaction: ExtendedStringSelectMenuInteraction];
+
+export const data: EventData = { type: 'on' };
+
+export const execute: EventExecute<Args> = async ({ client, rest: [interaction] }) => {
+	const guild = interaction.guild;
+	const channel = interaction.channel;
+	const member = interaction.member;
+	if (member.user.bot || !channel?.isTextBased()) return;
+
+	const granted = [];
+	const revoked = [];
+	for (const value of interaction.values) {
+		const role = guild.roles.cache.find(({ id }) => id === value);
+		if (!role) continue;
+
+		const restrictionsRole = guild.roles.cache.find(({ name }) => name === 'Restrictions');
+		if (restrictionsRole && member.roles.cache.has(restrictionsRole.id)) continue;
+
+		if (!member.roles.cache.has(role.id)) {
+			member.roles.add(role);
+			granted.push(role.name);
+			continue;
+		}
+
+		member.roles.remove(role);
+		revoked.push(role.name);
+	}
+
+	const embed = new EmbedBuilder()
+		.setTitle(`${granted.length} role(s) granted and ${revoked.length} role(s) revoked`)
+		.addFields([
+			{
+				name: 'Granted',
+				value: granted.join('\n') || 'None',
+				inline: true,
+			},
+			{
+				name: 'Revoked',
+				value: revoked.join('\n') || 'None',
+				inline: true,
+			},
+		])
+		.setColor('Random');
+
+	await interaction.reply({ embeds: [embed], ephemeral: true });
+
+	const name = member.nickname ?? member.displayName;
+	logger.info(`Roles updated for **${name}** in **${guild.name}** (+${granted.length} | -${revoked.length}).`);
+	logger.debug(JSON.stringify({ granted, revoked }));
+
+	client.emit('rolesMessageUpdate', client);
+};
---
+++ apps/bot/src/events/roles/rolesMessageUpdate.ts
@@ -0,0 +1,53 @@
+import { SettingsModel } from '@luferro/database';
+import { logger } from '@luferro/shared-utils';
+import { ActionRowBuilder, Collection, EmbedBuilder, Message, StringSelectMenuBuilder } from 'discord.js';
+
+import type { EventData, EventExecute } from '../../types/bot';
+import { Bot } from '../../structures/Bot';
+
+export const data: EventData = { type: 'on' };
+
+export const execute: EventExecute = async ({ client }) => {
+	for (const { 0: guildId, 1: guild } of client.guilds.cache) {
+		const settings = await SettingsModel.getSettingsByGuildId({ guildId });
+		const channelId = settings?.roles.channelId;
+		if (!channelId) continue;
+
+		const channel = await client.channels.fetch(channelId);
+		if (!channel?.isTextBased()) continue;
+
+		const roles = settings.roles.options
+			.map((id) => {
+				const role = guild.roles.cache.find(({ id: nestedRoleId }) => nestedRoleId === id);
+				return role ? { label: role.name, value: role.id } : null;
+			})
+			.filter((item): item is NonNullable<typeof item> => !!item);
+
+		const selectMenu = new StringSelectMenuBuilder()
+			.setCustomId(Bot.ROLES_MESSAGE_ID)
+			.setPlaceholder('Nothing selected.')
+			.setMaxValues(roles.length)
+			.addOptions(roles);
+		const component = new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(selectMenu);
+
+		const message = ((await channel.messages.fetch()) as Collection<string, Message>).find((message) => {
+			const customId = message?.components[0]?.components[0]?.customId;
+			return customId === selectMenu.data.custom_id;
+		});
+
+		if (message) {
+			await message.edit({ components: [component] });
+			logger.info(`Roles message updated for guild **${guild.name}**.`);
+			continue;
+		}
+
+		const embed = new EmbedBuilder()
+			.setTitle('Claim your roles!')
+			.setDescription('Use the select menu below to `claim` or `revoke` roles.')
+			.setFooter({ text: 'Each role grants access to a different text channel.' })
+			.setColor('Random');
+
+		await channel.send({ embeds: [embed], components: [component] });
+		logger.info(`Roles message sent to channelId **${channelId}** in guild **${guild.name}**.`);
+	}
+};
