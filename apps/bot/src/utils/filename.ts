290b1f2043e6bb2c1d71b6862a4de9826977d069
Luís Ferro <lfa1ferro@gmail.com>
2023-06-10 15:07:15 +0100

    Move utility to shared-utils package

--- apps/bot/src/utils/filename.ts
+++
@@ -1,9 +0,0 @@
-export const getCategoryFromPath = (path: string, splitPoint: string, suffix?: string) => {
-	const base =
-		path
-			.split(splitPoint)[1]
-			.match(/(?!(\\|\/))(.*?)(?=(\\|\/|\.))/g)
-			?.filter((match) => match)
-			.join('.') ?? '';
-	return suffix ? `${base}.${suffix}` : base;
-};
--- packages/shared-utils/src/utils/file.ts
+++ packages/shared-utils/src/utils/file.ts
@@ -1,6 +1,16 @@
 import fs from 'fs';
 import { join } from 'path';
 
+export const getRelativePath = (path: string, from: string, suffix?: string) => {
+	const base =
+		path
+			.split(from)[1]
+			.match(/(?!(\\|\/))(.*?)(?=(\\|\/|\.))/g)
+			?.filter((match) => match)
+			.join('.') ?? '';
+	return suffix ? `${base}.${suffix}` : base;
+};
+
 export const getFiles = (path: string, files: string[] = []) => {
 	for (const file of fs.readdirSync(path, { withFileTypes: true })) {
 		if (file.isDirectory()) {
