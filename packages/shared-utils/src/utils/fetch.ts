bba2bbb631a09d73b77435e921adcdd3318b45ab
Luís Ferro <lfa1ferro@gmail.com>
2023-06-10 15:05:22 +0100

    Replace undici for axios

--- packages/shared-utils/src/utils/fetch.ts
+++ packages/shared-utils/src/utils/fetch.ts
@@ -1,28 +1,32 @@
-import undici from 'undici';
-import UserAgent from 'user-agents';
+import axios from 'axios';
+import { HeaderGenerator, Headers } from 'header-generator';
 
 import { FetchError } from '../errors/FetchError';
 
 type HttpMethod = 'GET' | 'PUT' | 'POST' | 'PATCH' | 'DELETE';
-type Request = { url: string | URL; method?: HttpMethod; body?: string };
+type Request = { url: string | URL; method?: HttpMethod; authorization?: string; body?: string };
 
-export const fetch = async <T>({ method = 'GET', url, body }: Request) => {
-	const userAgent = { 'User-Agent': new UserAgent().toString() };
-	const headers =
-		method === 'POST' || method === 'PUT' ? { ...userAgent, 'content-type': 'application/json' } : userAgent;
+const getHeaders = ({ method, authorization }: Pick<Request, 'method' | 'authorization'>) => {
+	const custom = new Map();
+	if (method === 'POST' || method === 'PUT') custom.set('content-type', 'application/json');
+	if (authorization) custom.set('Authorization', `Bearer ${authorization}`);
 
+	return new HeaderGenerator().getHeaders({}, Object.fromEntries(custom));
+};
+
+export const fetch = async <T>({ method = 'GET', url, authorization, body }: Request) => {
 	try {
-		const res = await undici.request(url, { headers, method, body });
-		if (res.statusCode >= 400) {
-			await res.body.dump();
-			throw new Error(`Status code **${res.statusCode}**`);
-		}
+		const res = await axios(url.toString(), {
+			method,
+			headers: getHeaders({ method, authorization }),
+			data: body ? JSON.parse(body) : null,
+		});
+		if (res.status >= 400) throw new Error(`${res.status} ${res.statusText}`);
 
-		const payload: T = res.headers['content-type']?.includes('application/json')
-			? await res.body.json()
-			: await res.body.text();
+		const headers = res.headers as Headers;
+		const payload = res.data as T;
 
-		return { headers: res.headers, payload };
+		return { headers, payload };
 	} catch (error) {
 		throw new FetchError(`**${method}** request to **${url}** failed. Reason: ${(error as Error).message}`);
 	}
