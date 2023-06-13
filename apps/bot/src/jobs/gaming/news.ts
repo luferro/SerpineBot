f964330f63ad02e7fe0d057c38d5936c7572a325
Luís Ferro <lfa1ferro@gmail.com>
2023-06-13 19:24:48 +0100

    Mix gaming news from different sources

--- apps/bot/src/jobs/gaming/news.ts
+++ apps/bot/src/jobs/gaming/news.ts
@@ -1,18 +1,19 @@
 import { StringUtil } from '@luferro/shared-utils';
 import { EmbedBuilder } from 'discord.js';
 
+import { Bot } from '../../structures/Bot';
 import type { JobData, JobExecute } from '../../types/bot';
 
 export const data: JobData = { schedule: '0 */10 * * * *' };
 
 export const execute: JobExecute = async ({ client }) => {
-	const posts = await client.api.reddit.getPosts('Games', 'new', 25);
+	const data = [...(await getArticles(client)), ...(await getMedia(client))].sort(
+		(a, b) => b.publishedAt.getTime() - a.publishedAt.getTime(),
+	);
 
 	const embeds = [];
-	for (const { title, url, hasEmbeddedMedia, embedType, isSelf, isCrosspost } of posts.reverse()) {
-		if (isCrosspost || isSelf) continue;
-
-		if (embedType === 'youtube.com') {
+	for (const { title, url, image, isTwitterEmbed, isYoutubeEmbed } of data.reverse()) {
+		if (isYoutubeEmbed) {
 			const { channel } = await client.api.google.youtube.getVideoDetails(url);
 			if (typeof channel.subscribers === 'number' && channel.subscribers < 50_000) continue;
 		}
@@ -20,15 +21,43 @@ export const execute: JobExecute = async ({ client }) => {
 		const isSuccessful = await client.state({ title, url });
 		if (!isSuccessful) continue;
 
-		if (hasEmbeddedMedia) {
-			const content = `**${StringUtil.truncate(title)}**\n${url}`;
+		if (isTwitterEmbed || isYoutubeEmbed) {
+			const content = `**${title}**\n${isTwitterEmbed ? url.split('?')[0] : url}`;
 			await client.propageMessage({ category: 'Gaming News', content });
 			continue;
 		}
 
-		const embed = new EmbedBuilder().setTitle(StringUtil.truncate(title)).setURL(url).setColor('Random');
+		const embed = new EmbedBuilder()
+			.setTitle(StringUtil.truncate(title))
+			.setURL(url)
+			.setThumbnail(image)
+			.setColor('Random');
 		embeds.push(embed);
 	}
 
 	await client.propageMessages({ category: 'Gaming News', embeds });
+};
+
+const getArticles = async (client: Bot) => {
+	return (await client.api.gaming.news.getLatestNews()).map(({ title, url, image, publishedAt }) => ({
+		title,
+		url,
+		image,
+		publishedAt,
+		isYoutubeEmbed: false,
+		isTwitterEmbed: false,
+	}));
+};
+
+const getMedia = async (client: Bot) => {
+	return (await client.api.reddit.getPosts('Games', 'new', 25))
+		.filter(({ isCrosspost, isSelf, hasEmbeddedMedia }) => isCrosspost || isSelf || !hasEmbeddedMedia)
+		.map(({ title, url, embedType, publishedAt }) => ({
+			title,
+			url,
+			publishedAt,
+			image: null,
+			isYoutubeEmbed: embedType === 'youtube.com',
+			isTwitterEmbed: embedType === 'twitter.com',
+		}));
 };
