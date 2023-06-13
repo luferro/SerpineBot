f2edeb2271bc7e702729b13b0d96a6e9530c3e2a
Luís Ferro <lfa1ferro@gmail.com>
2023-06-13 16:33:17 +0100

    Change sources for gaming subscriptions catalogs

--- packages/gaming-api/src/api/subscriptions/subscriptions.scraper.ts
+++ packages/gaming-api/src/api/subscriptions/subscriptions.scraper.ts
@@ -1,60 +1,95 @@
 import { InteractiveScraper } from '@luferro/scraper';
 import { StringUtil } from '@luferro/shared-utils';
 
+type Catalog = { name: string; slug: string; url?: string | null };
+
 export enum Endpoint {
 	XBOX_GAME_PASS_CATALOG = 'https://www.xbox.com/pt-PT/xbox-game-pass/games',
-	PC_GAME_PASS_CATALOG = 'https://gg.deals/games/xbox-game-pass-pc-games-list',
-	EA_PLAY_CATALOG = 'https://gg.deals/games/ea-play-pc-games-list',
-	EA_PLAY_PRO_CATALOG = 'https://gg.deals/games/ea-play-pro-pc-games-list',
-	UBISOFT_PLUS_CATALOG = 'https://gg.deals/games/ubisoft-plus-games-list',
+	PC_GAME_PASS_CATALOG = 'https://www.xbox.com/pt-PT/xbox-game-pass/games#pcgames',
+	EA_PLAY_CATALOG = 'https://www.ea.com/ea-play/games#ea-app',
+	EA_PLAY_PRO_CATALOG = 'https://www.ea.com/ea-play/games#ea-play-pro',
+	UBISOFT_PLUS_CATALOG = 'https://store.ubisoft.com/ie/Ubisoftplus/games',
 }
 
 export const getCatalogList = async (url: Endpoint) => {
 	const { browser, page } = await InteractiveScraper.load({ url });
 	await page.waitForTimeout(5000);
 
-	const { element, locator, next } = getSelectors(url);
+	const { cookies, list, pagination } = getSelectors(url);
+	if (cookies) await page.locator(cookies).click();
 
-	const catalog = [];
+	const catalog: Catalog[] = [];
+
+	let currentPage = 1;
 	while (true) {
 		await page.waitForTimeout(2000);
 
-		const containers = await page.locator(locator).elementHandles();
+		const containers = await page.locator(list.element).elementHandles();
 		for (const container of containers) {
-			const name = await (await container.$(element.name))?.textContent();
-			const href = await (await container.$(element.href))?.getAttribute('href');
-			if (!name || !href || href.match(/dlc|pack/)) continue;
+			const name = await (await container.$(list.item.name))?.textContent();
+			const href = list.item.href ? await (await container.$(list.item.href))?.getAttribute('href') : null;
+			const url = list.item.base ? `${list.item.base}${href}` : href;
+			if (!name) continue;
 
-			catalog.push({
-				name,
-				slug: StringUtil.slug(name),
-				url: url === Endpoint.XBOX_GAME_PASS_CATALOG ? href : `https://gg.deals${href}`,
-			});
+			const isPresent = catalog.find((item) => item.name === name);
+			if (isPresent) continue;
+
+			// eslint-disable-next-line no-control-regex
+			catalog.push({ name: name.replace(/[^\x00-\x7F]/g, ''), slug: StringUtil.slug(name), url });
 		}
 
-		const nextPageButton = page.locator(next);
-		const hasMore = (await nextPageButton.count()) > 0;
+		const nextPageButton = page.locator(pagination.next).nth(pagination.nth);
+		const totalPages =
+			pagination.element && pagination.total
+				? Number((await page.locator(pagination.element).nth(pagination.nth).getAttribute(pagination.total))!)
+				: null;
+
+		const hasMore = totalPages ? totalPages !== currentPage : (await nextPageButton.count()) > 0;
 		if (!hasMore) break;
 
+		await nextPageButton.scrollIntoViewIfNeeded();
 		await nextPageButton.click();
+		currentPage++;
 	}
 	await InteractiveScraper.close({ browser });
 
-	return [...new Set(catalog)];
+	return catalog;
 };
 
 const getSelectors = (url: Endpoint) => {
-	if (url === Endpoint.XBOX_GAME_PASS_CATALOG) {
-		return {
-			locator: '.gameList [itemtype="http://schema.org/Product"]',
-			element: { name: 'h3', href: 'a' },
-			next: '.paginatenext:not(.pag-disabled) a',
-		};
-	}
+	const options = {
+		[Endpoint.XBOX_GAME_PASS_CATALOG]: {
+			list: {
+				element: '.gameList [itemtype="http://schema.org/Product"]',
+				item: { name: 'h3', href: 'a', base: null },
+			},
+			pagination: { nth: 0, element: null, total: null, next: '.paginatenext:not(.pag-disabled) a' },
+			cookies: null,
+		},
+		[Endpoint.PC_GAME_PASS_CATALOG]: {
+			list: {
+				element: '.gameList [itemtype="http://schema.org/Product"]',
+				item: { name: 'h3', href: 'a', base: null },
+			},
+			pagination: { nth: 0, element: null, total: null, next: '.paginatenext:not(.pag-disabled) a' },
+			cookies: null,
+		},
+		[Endpoint.EA_PLAY_CATALOG]: {
+			list: { element: 'ea-box-set ea-game-box', item: { name: 'a', href: 'a', base: 'https://www.ea.com' } },
+			pagination: { nth: 0, element: 'ea-pagination', total: 'total-pages', next: 'span[data-page-target=next]' },
+			cookies: null,
+		},
+		[Endpoint.EA_PLAY_PRO_CATALOG]: {
+			list: { element: 'ea-box-set ea-game-box', item: { name: 'a', href: 'a', base: 'https://www.ea.com' } },
+			pagination: { nth: 1, element: 'ea-pagination', total: 'total-pages', next: 'span[data-page-target=next]' },
+			cookies: null,
+		},
 
-	return {
-		locator: '.game-section [data-container-game-id]',
-		element: { name: '.game-info-wrapper a.game-info-title', href: 'a.full-link' },
-		next: 'li.next-page:not(li.next-page.disabled) a',
+		[Endpoint.UBISOFT_PLUS_CATALOG]: {
+			list: { element: 'div.game', item: { name: 'p.game-title', href: null, base: null } },
+			pagination: { nth: 0, element: null, total: null, next: '.game-list_wrapper ~ div button' },
+			cookies: '#privacy__modal__accept',
+		},
 	};
+	return options[url];
 };
