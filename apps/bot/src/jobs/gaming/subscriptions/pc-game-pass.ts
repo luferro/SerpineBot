f861b3bc51c575e6171f89ee1f179b67ede281ea
Luís Ferro <lfa1ferro@gmail.com>
2023-06-13 18:06:40 +0100

    Avoid mixing up xbox game pass and game pass for pc catalogs

--- apps/bot/src/jobs/gaming/subscriptions/pc-game-pass.ts
+++ apps/bot/src/jobs/gaming/subscriptions/pc-game-pass.ts
@@ -9,10 +9,18 @@ export const execute: JobExecute = async ({ client }) => {
 	const catalog = await client.api.gaming.subscriptions.getPcGamePassCatalog();
 	logger.debug(`Found **${catalog.length}** items in **Game Pass For PC** catalog.`);
 
+	let matches = 0;
 	const storedSubscription = await SubscriptionsModel.getCatalog({ category: 'Game Pass For PC' });
-	if (catalog.length < Math.round((storedSubscription?.count ?? 0) * 0.6)) {
-		logger.warn('Ignoring **Game Pass For PC** catalog...');
-		return;
+	if (storedSubscription) {
+		for (const { url } of storedSubscription.catalog) {
+			const isMatch = catalog.find((item) => item.url === url);
+			if (isMatch) matches++;
+		}
+
+		if (Math.round(catalog.length * 0.8) > matches) {
+			logger.warn('Ignoring **Game Pass For PC** catalog...');
+			return;
+		}
 	}
 
 	await SubscriptionsModel.updateCatalog({ category: 'Game Pass For PC', catalog });
