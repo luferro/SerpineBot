b139979b63ae4296c19d686a57ce10fdaf51cab0
Luís Ferro <lfa1ferro@gmail.com>
2023-06-17 23:26:51 +0100

    Change type definitions

---
+++ apps/web/types/bot.ts
@@ -0,0 +1,32 @@
+export type Option = {
+	type: number;
+	name: string;
+	description: string;
+	required: boolean;
+	choices?: { name: string }[];
+};
+
+export type Subcommand = {
+	type: number;
+	name: string;
+	description: string;
+	required: boolean;
+	options: Option[];
+};
+
+export type Group = {
+	type: number;
+	name: string;
+	description: string;
+	required: boolean;
+	subcommands: Subcommand[];
+	options: Option[];
+};
+
+export type Command = {
+	name: string;
+	description: string;
+	groups: Group[];
+	subcommands: Subcommand[];
+	options: Option[];
+};
