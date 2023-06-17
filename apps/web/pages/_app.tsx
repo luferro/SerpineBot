678192a31fa534639e33cdb0bdad07c747829ce7
Luís Ferro <lfa1ferro@gmail.com>
2023-06-17 23:27:36 +0100

    Migrate global styles to the provider

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,11 +1,35 @@
-import '../styles/globals.css';
-
 import { MantineProvider } from '@mantine/core';
+import { marked } from 'marked';
 import type { AppProps } from 'next/app';
+
+marked.setOptions({ mangle: false, headerIds: false });
 
 const App = ({ Component, pageProps }: AppProps) => {
 	return (
-		<MantineProvider withGlobalStyles withNormalizeCSS>
+		<MantineProvider
+			withGlobalStyles
+			withNormalizeCSS
+			theme={{
+				globalStyles: () => ({
+					'html,body': {
+						color: '#fff',
+						background: '#36393f',
+						padding: 0,
+						margin: 0,
+					},
+					'*': {
+						wordBreak: 'keep-all',
+						boxSizing: 'border-box',
+					},
+					'a': {
+						textDecoration: 'none',
+					},
+					'blockquote,p': {
+						margin: 0,
+					},
+				}),
+			}}
+		>
 			<Component {...pageProps} />
 		</MantineProvider>
 	);
