d953122fc1af7d7a9a3d2fc54c9d640a579eef0e
Luís Ferro <lfa1ferro@gmail.com>
2023-06-19 16:39:44 +0100

    Improve interactive scraper

--- packages/scraper/src/scraper/interactive.ts
+++ packages/scraper/src/scraper/interactive.ts
@@ -6,8 +6,10 @@ chromium.use(StealthPlugin());
 
 export const load = async ({ url }: { url: string }) => {
 	const browser = await chromium.launch();
+	const context = await browser.newContext();
+	await context.clearCookies();
 
-	const page = await browser.newPage();
+	const page = await context.newPage();
 	await page.route('**/*', (route: Route) => {
 		const hasResource = ['font', 'image'].some((resource) => resource === route.request().resourceType());
 		return hasResource ? route.abort() : route.continue();
@@ -24,4 +26,9 @@ export const getHtml = async ({ url }: { url: string }) => {
 	return html;
 };
 
-export const close = async ({ browser }: { browser: Browser }) => await browser.close();
+export const close = async ({ browser }: { browser: Browser }) => {
+	for (const context of browser.contexts()) {
+		await context.close();
+	}
+	await browser.close();
+};
