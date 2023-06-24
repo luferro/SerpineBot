c16f7ae95044a5294a4a9c0021c40eb34f1ae6d3
Luís Ferro <lfa1ferro@gmail.com>
2023-06-24 13:51:39 +0100

    Change news sources

--- packages/gaming-api/src/api/news/news.api.ts
+++ packages/gaming-api/src/api/news/news.api.ts
@@ -1,3 +1,11 @@
-import { Endpoint, getNewsList } from './news.scraper';
+import { EnumUtil } from '@luferro/shared-utils';
 
-export const getLatestNews = async () => await getNewsList(Endpoint.LATEST_NEWS);
+import { Feed, getNewsFeed } from './news.feed';
+
+export const getLatestNews = async () => {
+	const news: Awaited<ReturnType<typeof getNewsFeed>> = [];
+	for (const source of EnumUtil.enumKeysToArray(Feed)) {
+		news.concat(await getNewsFeed({ url: Feed[source] }));
+	}
+	return news.sort((a, b) => b.publishedAt.getTime() - a.publishedAt.getTime());
+};
---
+++ packages/gaming-api/src/api/news/news.feed.ts
@@ -0,0 +1,26 @@
+import { RSS, StaticScraper } from '@luferro/scraper';
+
+export enum Feed {
+	GAME_INFORMER = 'https://www.gameinformer.com/news.xml',
+	EUROGAMER = 'https://www.eurogamer.net/feed',
+	GAMESPOT = 'https://www.gamespot.com/feeds/game-news',
+	IGN = 'http://feeds.feedburner.com/ign/games-all',
+	VERGE = 'https://www.theverge.com/rss/games/index.xml',
+	ROCK_PAPER_SHOTGUN = 'https://www.rockpapershotgun.com/feed/news',
+}
+
+export const getNewsFeed = async ({ url }: { url: string }) => mapFeed({ data: await RSS.getFeed({ url }) });
+
+const mapFeed = ({ data }: { data: Awaited<ReturnType<typeof RSS.getFeed>> }) => {
+	return data.items.map(({ title, link, content, 'content:encoded': encodedContent, contentSnippet, isoDate }) => {
+		const $ = StaticScraper.loadHtml({ html: encodedContent ?? content });
+		const image = $('img').attr('src');
+		return {
+			title: title!,
+			url: link!,
+			description: contentSnippet!,
+			image: image!,
+			publishedAt: isoDate ? new Date(isoDate) : new Date(),
+		};
+	});
+};
