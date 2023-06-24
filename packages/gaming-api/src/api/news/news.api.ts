dc9a0e482464b37b360e0c61eb19f2e513eab301
Luís Ferro <lfa1ferro@gmail.com>
2023-06-24 17:35:35 +0100

    Fix fetch of gaming news

--- packages/gaming-api/src/api/news/news.api.ts
+++ packages/gaming-api/src/api/news/news.api.ts
@@ -3,9 +3,9 @@ import { EnumUtil } from '@luferro/shared-utils';
 import { Feed, getNewsFeed } from './news.feed';
 
 export const getLatestNews = async () => {
-	const news: Awaited<ReturnType<typeof getNewsFeed>> = [];
+	let news: Awaited<ReturnType<typeof getNewsFeed>> = [];
 	for (const source of EnumUtil.enumKeysToArray(Feed)) {
-		news.concat(await getNewsFeed({ url: Feed[source] }));
+		news = news.concat(await getNewsFeed({ url: Feed[source] }));
 	}
 	return news.sort((a, b) => b.publishedAt.getTime() - a.publishedAt.getTime());
 };
