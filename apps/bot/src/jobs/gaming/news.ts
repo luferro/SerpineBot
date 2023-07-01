b3cef356a5ebf1d54c79789db27cf307d9b98fa5
Luís Ferro <lfa1ferro@gmail.com>
2023-07-01 11:41:07 +0100

    Improve gaming news job

--- apps/bot/src/jobs/gaming/news.ts
+++ apps/bot/src/jobs/gaming/news.ts
@@ -1,15 +1,31 @@
 import { logger, StringUtil } from '@luferro/shared-utils';
 import { EmbedBuilder } from 'discord.js';
 
-import { Bot } from '../../structures/Bot';
 import type { JobData, JobExecute } from '../../types/bot';
 
 export const data: JobData = { schedule: '0 */10 * * * *' };
 
 export const execute: JobExecute = async ({ client }) => {
-	const data = [...(await getArticles(client)), ...(await getEmbeddedMedia(client))].sort(
-		(a, b) => b.publishedAt.getTime() - a.publishedAt.getTime(),
-	);
+	const data = [
+		...(await client.api.gaming.news.getLatestNews()).map(({ title, url, image, publishedAt }) => ({
+			title,
+			url,
+			image,
+			publishedAt,
+			isYoutubeEmbed: false,
+			isTwitterEmbed: false,
+		})),
+		...(await client.api.reddit.getPosts('Games', 'new', 25))
+			.filter(({ isCrosspost, isSelf }) => !isCrosspost && !isSelf)
+			.map(({ title, url, embedType, publishedAt }) => ({
+				title,
+				url,
+				publishedAt,
+				image: null,
+				isYoutubeEmbed: embedType === 'youtube.com',
+				isTwitterEmbed: embedType === 'twitter.com',
+			})),
+	].sort((a, b) => b.publishedAt.getTime() - a.publishedAt.getTime());
 
 	const embeds = [];
 	for (const { title, url, image, isTwitterEmbed, isYoutubeEmbed } of data.reverse()) {
@@ -18,7 +34,7 @@ export const execute: JobExecute = async ({ client }) => {
 				const { channel } = await client.api.google.youtube.getVideoDetails(url);
 				if (channel.subscribers < 50_000) continue;
 			} catch (error) {
-				logger.warn(`Failed to fetch subscribers for ${url}`);
+				logger.warn(`Failed to fetch subscribers for **${url}**.`);
 				continue;
 			}
 		}
@@ -42,28 +58,4 @@ export const execute: JobExecute = async ({ client }) => {
 	}
 
 	await client.propageMessages({ category: 'Gaming News', embeds });
-};
-
-const getArticles = async (client: Bot) => {
-	return (await client.api.gaming.news.getLatestNews()).map(({ title, url, image, publishedAt }) => ({
-		title,
-		url,
-		image,
-		publishedAt,
-		isYoutubeEmbed: false,
-		isTwitterEmbed: false,
-	}));
-};
-
-const getEmbeddedMedia = async (client: Bot) => {
-	return (await client.api.reddit.getPosts('Games', 'new', 25))
-		.filter(({ isCrosspost, isSelf, hasEmbeddedMedia }) => !isCrosspost && !isSelf && hasEmbeddedMedia)
-		.map(({ title, url, embedType, publishedAt }) => ({
-			title,
-			url,
-			publishedAt,
-			image: null,
-			isYoutubeEmbed: embedType === 'youtube.com',
-			isTwitterEmbed: embedType === 'twitter.com',
-		}));
 };
