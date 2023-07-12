fbc39bf5752e6669aee17fbd1a508c899587de68
Luís Ferro <lfa1ferro@gmail.com>
2023-07-12 16:30:43 +0100

    Remove unnecessary code

--- packages/gaming-api/src/index.ts
+++ packages/gaming-api/src/index.ts
@@ -1,8 +1,2 @@
-import { getReviewsDetails } from './api/reviews/reviews.scraper';
-
 // APIs
 export * as GamingApi from './api/gaming';
-
-(async () => {
-	console.log(await getReviewsDetails('https://opencritic.com/game/14514/pikmin-4'));
-})();
