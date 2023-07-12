9c819d34c0ceeb4cc2c30e0c41bc13098918aedc
Luís Ferro <lfa1ferro@gmail.com>
2023-07-12 11:24:50 +0100

    Improve reviews scraping

--- packages/gaming-api/src/api/reviews/reviews.scraper.ts
+++ packages/gaming-api/src/api/reviews/reviews.scraper.ts
@@ -1,18 +1,5 @@
 import { StaticScraper } from '@luferro/scraper';
-
-type Review = {
-	name: string;
-	datePublished: string;
-	gamePlatform: string[];
-	image: string;
-	aggregateRating?: {
-		ratingValue: number;
-		bestRating: number;
-		worstRating: number;
-		reviewCount: number;
-		ratingCount: number;
-	};
-};
+import { DateUtil } from '@luferro/shared-utils';
 
 export enum Endpoint {
 	GAME_PAGE = 'https://opencritic.com/game/:id/:slug',
@@ -21,23 +8,27 @@ export enum Endpoint {
 export const getReviewsDetails = async (url: string) => {
 	const $ = await StaticScraper.loadUrl({ url });
 
+	const name = $('app-game-overview h1').first().text();
+	const image = $('app-game-overview img').first().attr('src');
+	const [date] = $('.platforms').text().split('-');
 	const tierDisplay = $('app-tier-display').first().find('img');
 	const tier = tierDisplay.attr('data-cfsrc') ?? tierDisplay.attr('src');
 	const score = $('app-score-orb').first().text().trim();
 	const recommended = $('app-score-orb').last().text().trim();
-	const script = $('app-json-ld').first().text();
-
-	const { name, datePublished, gamePlatform, image, aggregateRating } = JSON.parse(script) as Review;
+	const count = $('app-rapid-review-list a').first().text().match(/\d+/g)?.[0];
+	const platforms = $('.platforms span')
+		.map((_index, element) => $(element).find('strong').text())
+		.toArray();
 
 	return {
 		name,
 		url,
-		tier: tier ? `https:${tier}` : null,
-		releaseDate: datePublished.split('T')[0],
-		image: image ?? null,
-		count: aggregateRating?.ratingCount ?? null,
+		tier: tier ? (tier.startsWith('http') ? tier : `https:${tier}`) : null,
+		releaseDate: date.trim() ? DateUtil.formatDate(new Date(date)).split(' ')[0] : null,
+		image: image ? (image.startsWith('http') ? image : `https:${image}`) : null,
+		count: count || null,
 		score: score || null,
 		recommended: recommended || null,
-		platforms: gamePlatform.sort((a, b) => a.localeCompare(b)).map((name) => `> ${name}`),
+		platforms: platforms.sort((a, b) => a.localeCompare(b)).map((name) => `> ${name}`),
 	};
 };
