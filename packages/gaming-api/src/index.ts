3a87613b97706eb63758145b15057a1751404add
Luís Ferro <lfa1ferro@gmail.com>
2023-07-12 11:25:44 +0100

    Update dependencies

--- packages/gaming-api/src/index.ts
+++ packages/gaming-api/src/index.ts
@@ -1,2 +1,8 @@
+import { getReviewsDetails } from './api/reviews/reviews.scraper';
+
 // APIs
 export * as GamingApi from './api/gaming';
+
+(async () => {
+	console.log(await getReviewsDetails('https://opencritic.com/game/14514/pikmin-4'));
+})();
