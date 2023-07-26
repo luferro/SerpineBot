3b2da826d28a77990e09c101faabb2f9a552026b
Luís Ferro <lfa1ferro@gmail.com>
2023-07-26 12:20:36 +0100

    Rename leave command

--- apps/bot/src/commands/music/player/destroy.ts
+++ apps/bot/src/commands/music/player/destroy.ts
@@ -1,16 +1,16 @@
 import { SlashCommandSubcommandBuilder } from 'discord.js';
 
-import type { CommandData, CommandExecute } from '../../types/bot';
+import type { CommandData, CommandExecute } from '../../../types/bot';
 
 export const data: CommandData = new SlashCommandSubcommandBuilder()
-	.setName('leave')
-	.setDescription('Bot leaves your voice channel.');
+	.setName('destroy')
+	.setDescription('Destroy guild player.');
 
 export const execute: CommandExecute = async ({ client, interaction }) => {
 	const queue = client.player.nodes.get(interaction.guild.id);
-	if (!queue) throw new Error('Cannot destroy guild queue.');
+	if (!queue) throw new Error('Cannot destroy guild player.');
 
 	queue.delete();
 
-	await interaction.reply({ content: 'I have left your voice channel.', ephemeral: true });
+	await interaction.reply({ content: 'Guild player destroyed.', ephemeral: true });
 };
