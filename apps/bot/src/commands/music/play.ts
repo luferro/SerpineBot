1724a3b10ddec2e954b7f24b72a64476238900d2
Luís Ferro <lfa1ferro@gmail.com>
2023-07-26 12:23:34 +0100

    Improve play command

--- apps/bot/src/commands/music/play.ts
+++ apps/bot/src/commands/music/play.ts
@@ -5,32 +5,38 @@ import type { CommandData, CommandExecute } from '../../types/bot';
 
 export const data: CommandData = new SlashCommandSubcommandBuilder()
 	.setName('play')
-	.setDescription('Plays the first result matching your search query.')
-	.addStringOption((option) => option.setName('query').setDescription('Youtube search query.').setRequired(true));
+	.setDescription('Plays / enqueues track matching your query.')
+	.addStringOption((option) => option.setName('query').setDescription('Track query.').setRequired(true));
 
 export const execute: CommandExecute = async ({ client, interaction }) => {
-	await interaction.deferReply();
+	await interaction.deferReply({ ephemeral: true });
 
 	const query = interaction.options.getString('query', true);
 
-	const { tracks, playlist } = await client.player.search(query, {
+	const channel = interaction.member.voice.channel;
+	if (!channel) throw new Error('You are not in a voice channel.');
+
+	const {
+		track,
+		queue,
+		searchResult: { playlist },
+	} = await client.player.play(channel, query, {
 		requestedBy: interaction.user,
 		searchEngine: QueryType.AUTO,
+		nodeOptions: {
+			metadata: interaction.channel,
+			leaveOnEmpty: true,
+			leaveOnEmptyCooldown: 1000 * 60 * 5,
+			leaveOnEnd: false,
+		},
 	});
-	const filteredTracks = tracks.filter(({ duration }) => !/0?0:00/.test(duration));
-	if (!playlist && filteredTracks.length === 0) throw new Error(`No matches for "${query}".`);
 
-	const queue = await forceJoin({ client, interaction });
-	queue.addTrack(playlist ?? filteredTracks[0]);
-	if (!queue.node.isPlaying()) await queue.node.play();
-
-	const position = queue.node.getTrackPosition(playlist?.tracks[0] ?? filteredTracks[0]) + 1;
-
+	const position = queue.node.getTrackPosition(playlist?.tracks[0] ?? track) + 1;
 	const embed = new EmbedBuilder()
-		.setAuthor({ name: playlist?.author.name ?? filteredTracks[0].author })
-		.setTitle(playlist?.title ?? filteredTracks[0].title)
-		.setURL(playlist?.url ?? filteredTracks[0].url)
-		.setThumbnail(playlist?.thumbnail ?? filteredTracks[0].thumbnail)
+		.setAuthor({ name: playlist?.author.name ?? track.author })
+		.setTitle(playlist?.title ?? track.title)
+		.setURL(playlist?.url ?? track.url)
+		.setThumbnail(playlist?.thumbnail ?? track.thumbnail)
 		.addFields([
 			{
 				name: '**Position**',
@@ -39,23 +45,11 @@ export const execute: CommandExecute = async ({ client, interaction }) => {
 			},
 			{
 				name: '**Duration**',
-				value: playlist?.durationFormatted ?? filteredTracks[0].duration,
+				value: playlist?.durationFormatted ?? track.duration,
 				inline: true,
 			},
 		])
 		.setColor('Random');
 
 	await interaction.editReply({ embeds: [embed] });
-};
-
-const forceJoin = async ({ client, interaction }: Parameters<typeof execute>[0]) => {
-	let queue = client.player.nodes.get(interaction.guild.id);
-	if (!queue) {
-		const memberVoiceChannel = interaction.member.voice.channel;
-		if (!memberVoiceChannel) throw new Error('You are not in a voice channel.');
-
-		queue = client.player.nodes.create(interaction.guild.id, { leaveOnEmptyCooldown: 1000 * 60 * 5 });
-		await queue.connect(memberVoiceChannel);
-	}
-	return queue;
 };
