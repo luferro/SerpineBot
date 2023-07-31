c6a929de9a14946e419bd940632dab59abb6fb8d
Luís Ferro <lfa1ferro@gmail.com>
2023-07-31 18:55:04 +0100

    Fix event triggered when a track gets added to the queue

--- apps/bot/src/events/player/audioTrackAdd.ts
+++ apps/bot/src/events/player/audioTrackAdd.ts
@@ -7,19 +7,37 @@ type Args = [queue: GuildQueue<TextBasedChannel>, track: Track];
 
 export const data: EventData = { type: 'on' };
 
-export const execute: EventExecute<Args> = async ({ rest: [queue, track] }) => {
-	const { metadata, history, currentTrack } = queue;
-	if (!!currentTrack && currentTrack.id === history.currentTrack?.id) return;
+export const execute: EventExecute<Args> = async ({ client, rest: [queue, track] }) => {
+	const { metadata, currentTrack } = queue;
 
-	const embed = new EmbedBuilder()
-		.setAuthor({
-			iconURL: track.requestedBy?.avatarURL() ?? track.requestedBy?.defaultAvatarURL,
-			name: 'Added track',
-		})
-		.setTitle(track.title)
-		.setURL(track.url)
-		.setThumbnail(track.thumbnail)
-		.setColor('Random');
+	const embed = new EmbedBuilder();
+	const isTrackPlaying = !currentTrack && queue.tracks.size === 1;
+	if (isTrackPlaying) {
+		embed
+			.setAuthor({
+				iconURL: client.user?.avatarURL() ?? client.user?.defaultAvatarURL,
+				name: 'Now playing',
+			})
+			.setTitle(track.title)
+			.setURL(track.url)
+			.setThumbnail(track.thumbnail)
+			.setFields([{ name: '**Duration**', value: `**${track.duration}**` }])
+			.setFooter({
+				iconURL: track.requestedBy?.avatarURL() ?? track.requestedBy?.defaultAvatarURL,
+				text: `Requested by ${track.requestedBy?.username}`,
+			})
+			.setColor('Random');
+	} else {
+		embed
+			.setAuthor({
+				iconURL: track.requestedBy?.avatarURL() ?? track.requestedBy?.defaultAvatarURL,
+				name: 'Added track',
+			})
+			.setTitle(track.title)
+			.setURL(track.url)
+			.setThumbnail(track.thumbnail)
+			.setColor('Random');
+	}
 
-	metadata.send({ embeds: [embed] });
+	await metadata.send({ embeds: [embed] });
 };
