e73bef425b601144db981a589745962bc33bacd9
Luís Ferro <lfa1ferro@gmail.com>
2023-11-03 17:58:01 +0000

    Change choices values

--- apps/bot/src/commands/interactions/gaming/xbox/charts.ts
+++ apps/bot/src/commands/interactions/gaming/xbox/charts.ts
@@ -11,9 +11,9 @@ export const data: InteractionCommandData = new SlashCommandSubcommandBuilder()
 			.setName(t('interactions.gaming.xbox.charts.options.0.name'))
 			.setDescription(t('interactions.gaming.xbox.charts.options.0.description'))
 			.addChoices(
-				{ name: t('interactions.gaming.xbox.charts.options.0.choices.0.name'), value: 1 },
-				{ name: t('interactions.gaming.xbox.charts.options.0.choices.1.name'), value: 2 },
-				{ name: t('interactions.gaming.xbox.charts.options.0.choices.2.name'), value: 3 },
+				{ name: t('interactions.gaming.xbox.charts.options.0.choices.0.name'), value: 0 },
+				{ name: t('interactions.gaming.xbox.charts.options.0.choices.1.name'), value: 1 },
+				{ name: t('interactions.gaming.xbox.charts.options.0.choices.2.name'), value: 2 },
 			)
 			.setRequired(true),
 	);
@@ -29,7 +29,7 @@ export const execute: InteractionCommandExecute = async ({ client, interaction }
 	const embed = new EmbedBuilder()
 		.setTitle(
 			t('interactions.gaming.xbox.top.embed.title', {
-				chart: t(`interactions.gaming.xbox.charts.options.0.choices.${chart - 1}.name`),
+				chart: t(`interactions.gaming.xbox.charts.options.0.choices.${chart}.name`),
 			}),
 		)
 		.setDescription(data.map(({ position, name, url }) => `\`${position}.\` [${name}](${url})`).join('\n'))
