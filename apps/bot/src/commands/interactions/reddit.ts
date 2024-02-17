90527d6e1a3c393eaea77075b0e8065071d515d4
luferro <lfa1ferro@gmail.com>
2024-02-17 11:32:21 +0000

    Replace memes command with a broader command

--- apps/bot/src/commands/interactions/reddit.ts
+++ apps/bot/src/commands/interactions/reddit.ts
@@ -1,14 +1,23 @@
 import { StringUtil } from "@luferro/shared-utils";
-import { EmbedBuilder } from "discord.js";
+import { EmbedBuilder, SlashCommandStringOption } from "discord.js";
 import { t } from "i18next";
 
-import type { InteractionCommandExecute } from "../../types/bot";
+import type { InteractionCommandData, InteractionCommandExecute } from "../../types/bot";
+
+export const data: InteractionCommandData = [
+	new SlashCommandStringOption()
+		.setName(t("interactions.reddit.options.0.name"))
+		.setDescription(t("interactions.reddit.options.0.description"))
+		.setRequired(true),
+];
 
 export const execute: InteractionCommandExecute = async ({ client, interaction }) => {
 	await interaction.deferReply();
 
-	const posts = await client.api.reddit.getPosts({ subreddit: "Memes" });
-	const filteredPosts = posts.filter((post) => !post.isSelf);
+	const subreddit = interaction.options.getString(data[0].name, true);
+
+	const posts = await client.api.reddit.getPosts({ subreddit });
+	const filteredPosts = posts.filter((post) => !post.isSelf && !post.isNsfw);
 	if (filteredPosts.length === 0) throw new Error(t("errors.search.none"));
 
 	const { title, url, selfurl, hasEmbeddedMedia } = filteredPosts[Math.floor(Math.random() * filteredPosts.length)];
