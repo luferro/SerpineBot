e717ff86e2653fee4438ddfb45cf186f923a50ad
luferro <lfa1ferro@gmail.com>
2024-02-17 11:30:10 +0000

    Merge getPosts methods into one

--- packages/reddit-api/lib/reddit.api.ts
+++ packages/reddit-api/lib/reddit.api.ts
@@ -1,15 +1,6 @@
 import { Scraper } from "@luferro/scraper";
 import { FetchUtil } from "@luferro/shared-utils";
 
-type Url = { url: string };
-type Subreddit = { subreddit: string };
-type Sort = { sort: "new" | "hot" | "top" };
-type Flairs = { flairs: string[] };
-type Limit = { limit: number };
-
-type PostsQuery = Subreddit & Partial<Sort> & Partial<Limit>;
-type PostsWithFlairsQuery = PostsQuery & Flairs;
-
 type Payload<T> = { data: T };
 type Children<T> = { children: T };
 
@@ -27,13 +18,33 @@ type Post = {
 		is_video: boolean;
 		removed_by_category: boolean | null;
 		created_utc: number;
+		over_18: boolean;
 	};
+};
+
+type PostsOptions = {
+	subreddit: string;
+	flairs?: string[];
+	/**
+	 * @default "hot"
+	 */
+	sort?: "new" | "hot" | "top";
+	/**
+	 * @default 100
+	 */
+	limit?: number;
 };
 
 export class RedditApi extends Scraper {
 	private static BASE_URL = "https://old.reddit.com";
 
-	private async getPostsByUrl({ url }: Url) {
+	async getPosts({ subreddit, flairs = [], sort = "hot", limit = 100 }: PostsOptions) {
+		let url = `${RedditApi.BASE_URL}/r/${subreddit}/${sort}.json?limit=${limit}&restrict_sr=1`;
+		if (flairs.length > 0) {
+			const flair = flairs.map((flair) => `flair_name:"${flair}"`).join(" OR ");
+			url = `${RedditApi.BASE_URL}/r/${subreddit}/search.json?q=${flair}&limit=${limit}&sort=${sort}&restrict_sr=1`;
+		}
+
 		const { payload } = await FetchUtil.fetch<Payload<Children<Post[]>>>({ url });
 		if (!payload?.data?.children) throw new Error("Failed to retrieve reddit post.");
 
@@ -54,26 +65,12 @@ export class RedditApi extends Scraper {
 					selfurl: `${RedditApi.BASE_URL}${data.permalink}`,
 					isCrosspost: Boolean(data.crosspost_parent),
 					isSelf: data.is_self,
+					isNsfw: data.over_18,
 					isYoutubeEmbed: data.secure_media?.type === "youtube.com" || this.youtube.isVideo({ url: data.url }),
 					gallery: data.gallery_data,
 					publishedAt: new Date(data.created_utc),
 					hasEmbeddedMedia: Boolean(data.secure_media) || /\.(gif|gifv|mp4)/.test(data.url),
 				};
 			});
-	}
-
-	async getPosts({ subreddit, sort = "hot", limit = 100 }: PostsQuery) {
-		return this.getPostsByUrl({
-			url: `${RedditApi.BASE_URL}/r/${subreddit}/${sort}.json?limit=${limit}&restrict_sr=1`,
-		});
-	}
-
-	async getPostsByFlair({ subreddit, flairs, sort = "hot", limit = 100 }: PostsWithFlairsQuery) {
-		if (flairs.length === 0) throw new Error("Empty flair array.");
-		const flair = flairs.map((flair) => `flair_name:"${flair}"`).join(" OR ");
-
-		return this.getPostsByUrl({
-			url: `${RedditApi.BASE_URL}/r/${subreddit}/search.json?q=${flair}&limit=${limit}&sort=${sort}&restrict_sr=1`,
-		});
 	}
 }
