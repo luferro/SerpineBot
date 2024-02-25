549928b4a7ac03086e040b665c6c14ea40a6a216
luferro <lfa1ferro@gmail.com>
2024-02-25 21:43:29 +0000

    Update anime job message

--- apps/bot/src/jobs/anime/episode.ts
+++ apps/bot/src/jobs/anime/episode.ts
@@ -9,70 +9,49 @@ export const data: JobData = { schedule: "0 */5 * * * *" };
 
 export const execute: JobExecute = async ({ client }) => {
 	const schedule = (await client.api.anime.getWeeklySchedule()).filter(
-		(anime) => new Date(anime.episodes.current.date).getDay() === new Date().getDay(),
+		(anime) => new Date(anime.airing.sub.at ?? anime.airing.raw.at).getDay() === new Date().getDay(),
 	);
 
-	const translate = (name: string) => {
-		if (name === "mal") return "MyAnimeList";
-		if (name === "hidive") return name.toUpperCase();
-		return StringUtil.capitalize(name);
-	};
-
 	const messages = [];
-	for (const { id, titles, url, image, episodes, streams, isDelayed } of schedule) {
-		const notAiredYet = Date.now() < new Date(episodes.current.date).getTime();
-		if (notAiredYet || isDelayed) continue;
+	for (const anime of schedule) {
+		const { title, coverImage, format, score, airing, episodes, duration, streams, trackers, isDelayed } = anime;
 
-		const { score, season, trackers } = await client.api.anime.getAnimeById({ id });
+		const notAiredYet = Date.now() < new Date(airing.sub.at ?? airing.raw.at).getTime();
+		if (notAiredYet || isDelayed || !title.romaji) continue;
 
-		const formattedTracking = trackers?.map(({ tracker, url }) => `> **[${translate(tracker)}](${url})**`);
-		const formattedStreams = streams?.map(({ stream, url }) => `> **[${translate(stream)}](${url})**`);
+		const type = airing.sub.at ? "SUB" : "RAW";
+		const formattedTracking = trackers?.map(({ name, url }) => `> **[${StringUtil.capitalize(name)}](${url})**`);
+		const formattedStreams = streams?.map(({ name, url }) => `> **[${StringUtil.capitalize(name)}](${url})**`);
 
 		const embed = new EmbedBuilder()
-			.setTitle(
-				t("jobs.anime.episode.embed.title", {
-					anime: StringUtil.truncate(titles.default, 240),
-					episode: episodes.current.number,
-				}),
-			)
-			.setURL(url)
-			.setDescription(season ? `*${season}*` : null)
-			.setThumbnail(image)
+			.setTitle(StringUtil.truncate(title.english ?? title.romaji))
+			.setDescription(t("jobs.anime.episode.embed.description", { episode: airing.episode, type }))
+			.setThumbnail(coverImage.extraLarge ?? coverImage.large ?? coverImage.medium)
 			.addFields([
 				{
 					name: t("jobs.anime.episode.embed.fields.0.name"),
-					value: titles.alternative ?? t("common.unavailable"),
+					value: score ? `${score} / 10` : t("common.unavailable"),
 				},
 				{
 					name: t("jobs.anime.episode.embed.fields.1.name"),
-					value: episodes.total
-						? t("jobs.anime.episode.embed.fields.1.value", { episodes: episodes.total })
-						: t("common.unavailable"),
-					inline: true,
-				},
-				{
-					name: t("jobs.anime.episode.embed.fields.2.name"),
-					value: episodes.duration
-						? t("jobs.anime.episode.embed.fields.2.value", { duration: episodes.duration })
-						: t("common.unavailable"),
-					inline: true,
-				},
-				{
-					name: t("jobs.anime.episode.embed.fields.3.name"),
-					value: score?.toString() ?? t("common.unavailable"),
-					inline: true,
-				},
-				{
-					name: t("jobs.anime.episode.embed.fields.4.name"),
 					value: formattedStreams.join("\n") || t("common.unavailable"),
 					inline: true,
 				},
 				{
-					name: t("jobs.anime.episode.embed.fields.5.name"),
+					name: t("jobs.anime.episode.embed.fields.2.name"),
 					value: formattedTracking.join("\n") || t("common.unavailable"),
 					inline: true,
 				},
 			])
+			.setFooter({
+				text: [
+					episodes ? t("common.episodes", { episodes }) : null,
+					format,
+					duration ? t("common.time.minutes", { minutes: duration }) : null,
+				]
+					.filter((item) => !!item)
+					.join(" • "),
+			})
 			.setColor("Random");
 
 		messages.push(embed);
