e8e877c040e8f4e404f7858b21439adcf0ac4126
luferro <lfa1ferro@gmail.com>
2024-02-29 18:19:30 +0000

    Fix free deals job

--- apps/bot/src/jobs/gaming/deals/free.ts
+++ apps/bot/src/jobs/gaming/deals/free.ts
@@ -3,20 +3,39 @@ import { EmbedBuilder } from "discord.js";
 import { t } from "i18next";
 
 import type { JobData, JobExecute } from "../../../types/bot";
+import { ConverterUtil, DateUtil } from "@luferro/shared-utils";
 
 export const data: JobData = { schedule: "0 */10 * * * *" };
 
 export const execute: JobExecute = async ({ client }) => {
+	const localization = client.getLocalization();
+
 	const messages = [];
 	const { feeds } = await client.prisma.config.getWebhookConfig({ webhook: WebhookType.FREE_GAMES });
 
 	const freebies = await client.api.gaming.games.deals.getFreebies();
 	for (const { title, url, discount, regular, store, expiry } of freebies.reverse()) {
+		const { amount, currency } = regular;
+
 		const embed = new EmbedBuilder()
 			.setTitle(title)
 			.setURL(url)
-			.setDescription(t("jobs.gaming.deals.free.embed.description", { discount, regular, store }))
-			.setFooter(expiry ? { text: t("jobs.gaming.deals.free.embed.footer.text", { expiry }) } : null)
+			.setDescription(
+				t("jobs.gaming.deals.free.embed.description", {
+					store,
+					discount,
+					regular: ConverterUtil.formatCurrency({ amount, currency, ...localization }),
+				}),
+			)
+			.setFooter(
+				expiry
+					? {
+							text: t("jobs.gaming.deals.free.embed.footer.text", {
+								expiry: DateUtil.format({ date: expiry, ...localization }),
+							}),
+					  }
+					: null,
+			)
 			.setColor("Random");
 
 		messages.push(embed);
