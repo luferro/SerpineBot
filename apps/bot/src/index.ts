a59943ef333123514a09a96d4e90b93e6d5cb501
luferro <lfa1ferro@gmail.com>
2024-05-06 22:42:21 +0100

    Bind pino logger

--- apps/bot/src/index.ts
+++ apps/bot/src/index.ts
@@ -14,6 +14,6 @@ const client = new Bot({
 });
 await client.start();
 
-process.on("uncaughtException", client.logger.error);
+process.on("uncaughtException", client.logger.error.bind(client.logger));
 process.on("SIGINT", client.stop);
 process.on("SIGTERM", client.stop);
