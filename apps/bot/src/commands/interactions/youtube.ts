9941ffb43b31c3dac0108a7d832dbb1ff75ffc8c
luferro <lfa1ferro@gmail.com>
2024-08-04 00:20:58 +0100

    Introduce auto complete feature to youtube search command

--- apps/bot/src/commands/interactions/youtube.ts
+++ apps/bot/src/commands/interactions/youtube.ts
@@ -1,6 +1,7 @@
+import { truncate } from "@luferro/helpers/transform";
 import { SlashCommandStringOption } from "discord.js";
 import { t } from "i18next";
-import type { InteractionCommandData, InteractionCommandExecute } from "~/types/bot.js";
+import type { InteractionCommandAutoComplete, InteractionCommandData, InteractionCommandExecute } from "~/types/bot.js";
 
 export const data: InteractionCommandData = [
 	new SlashCommandStringOption()
@@ -17,4 +18,20 @@ export const execute: InteractionCommandExecute = async ({ client, interaction }
 	if (results.isEmpty()) throw new Error(t("errors.search.lookup", { query }));
 
 	await interaction.editReply({ content: results.tracks[0].url });
+};
+
+export const autocomplete: InteractionCommandAutoComplete = async ({ client, interaction }) => {
+	const { value: query } = interaction.options.getFocused(true);
+	if (query.length < 3) return interaction.respond([]);
+
+	const results = await client.player.search(query);
+
+	await interaction.respond(
+		results.tracks.slice(0, 10).map((item) => {
+			const author = item.author;
+			const duration = item.duration;
+			const limit = 100 - author.length - duration.length - 6;
+			return { name: `${author} - ${truncate(item.title, limit)} | ${duration}`, value: item.url };
+		}),
+	);
 };
