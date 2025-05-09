b62de311c3ddb4f35864ef6afad6afc1ca343885
luferro <lfa1ferro@gmail.com>
2025-05-10 00:33:05 +0100

    Update drizzle config

--- apps/memberberries/bot/drizzle.config.ts
+++ apps/memberberries/bot/drizzle.config.ts
@@ -1,13 +1,10 @@
-import { loadConfig } from "@luferro/config";
 import { defineConfig } from "drizzle-kit";
-
-const config = loadConfig();
 
 export default defineConfig({
 	out: "./drizzle",
 	dialect: "postgresql",
 	schema: "./src/db/schema.ts",
 	dbCredentials: {
-		url: config.get("services.postgres.uri").concat("/memberberries"),
+		url: process.env.SB_SERVICES__POSTGRES__URI?.concat("/memberberries") ?? "",
 	},
 });
